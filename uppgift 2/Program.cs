031b07ce14c2058168f2c54cd92833657def5465
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-09-12 14:17:19 +0200

    Add project files.

---
+++ uppgift 2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Uppgift_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Din dator har kraschad");
+        }
+    }
+}
